df026cbeb2889d57ddc1c2dded5b62b3eb98645f
KengoWada <louiskengo3@gmail.com>
2025-02-22 19:26:58 +0300

    Add simple store package

---
+++ internal/store/store.go
@@ -0,0 +1,26 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Store struct{}
+
+func NewStorage(db *sql.DB) Store {
+	return Store{}
+}
+
+func WithTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
+}
