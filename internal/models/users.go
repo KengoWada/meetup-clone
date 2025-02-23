1595e140394aa0badc296ade69ea7d9621f0aab1
KengoWada <louiskengo3@gmail.com>
2025-02-23 22:14:06 +0300

    Add user and user profile models

---
+++ internal/models/users.go
@@ -0,0 +1,27 @@
+package models
+
+type UserRole string
+
+const (
+	UserAdminRole  UserRole = "admin"
+	UserStaffRole  UserRole = "staff"
+	UserClientRole UserRole = "client"
+)
+
+type User struct {
+	BaseModel
+	Email              string   `json:"email"`
+	Password           string   `json:"-"`
+	IsActive           bool     `json:"-"`
+	Role               UserRole `json:"role"`
+	PasswordResetToken string   `json:"-"`
+}
+
+type UserProfile struct {
+	BaseModel
+	Username    string `json:"username"`
+	ProfilePic  string `json:"profilePic"`
+	DateOfBirth string `json:"dateOfBirth"`
+	UserID      int64  `json:"userId"`
+	User        *User  `json:"json"`
+}
