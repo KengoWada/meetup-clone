9a7075a9b6f3921791199afbd407b0933baee1e6
KengoWada <louiskengo3@gmail.com>
2025-02-23 22:25:30 +0300

    Add register user validation error response messages

---
+++ internal/services/auth/validation.go
@@ -0,0 +1,20 @@
+package auth
+
+import "github.com/KengoWada/meetup-clone/internal/utils"
+
+var (
+	registerUserPayloadErrors = utils.FieldErrorMessages{
+		"email": {"email": "Invalid email address provided"},
+		"password": {
+			"min":         "Password must have at least 10 characters",
+			"max":         "Password must have at most 72 characters",
+			"is_password": "Password must contain a number, lower case character, upper case character and one of the special symbols(including space) !@#$%^&*()-_+=,.?|\\/<>[]{}",
+		},
+		"username": {
+			"min": "Username must have at least 3 characters",
+			"max": "Username must have at most 100 characters",
+		},
+		"profilePic":  {"http_url": "Invalid URL format"},
+		"dateOfBirth": {"is_date": "Invalid date format. mm/dd/yyyy"},
+	}
+)
