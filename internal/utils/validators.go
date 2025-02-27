0e7c530b64fdcdf0ae47d9427b19c6bcd809f61c
KengoWada <louiskengo3@gmail.com>
2025-02-27 20:46:11 +0300

    Add payload validator helper

--- internal/utils/validators.go
+++ internal/utils/validators.go
@@ -4,13 +4,30 @@ import (
 	"errors"
 	"unicode"
 
+	"github.com/KengoWada/meetup-clone/internal/validate"
 	"github.com/go-playground/validator/v10"
 )
 
 type TagErrorMessages map[string]string
 type FieldErrorMessages map[string]TagErrorMessages
 
-func GenerateErrorMessages(err error, errorMessages FieldErrorMessages) (map[string]string, error) {
+var ErrFailedValidation = errors.New("payload failed validation")
+
+func ValidatePayload(payload any, errorMessages FieldErrorMessages) (map[string]string, error) {
+	err := validate.Validate.Struct(payload)
+	if err == nil {
+		return nil, nil
+	}
+
+	errResponse, err := generateErrorMessages(err, errorMessages)
+	if err != nil {
+		return nil, err
+	}
+
+	return errResponse, ErrFailedValidation
+}
+
+func generateErrorMessages(err error, errorMessages FieldErrorMessages) (map[string]string, error) {
 	var validateErrs validator.ValidationErrors
 	var response = make(map[string]string)
 
