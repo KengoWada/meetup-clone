c0e014fb76d4e521a1713e46b2bbe41120ae5a94
KengoWada <louiskengo3@gmail.com>
2025-02-27 20:34:32 +0300

    Add NewConfig and app configs

---
+++ internal/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"slices"
+
+	"github.com/KengoWada/meetup-clone/internal/utils"
+	"github.com/rs/zerolog"
+)
+
+const (
+	AppEnvDev  AppEnv = "dev"
+	AppEnvTest AppEnv = "test"
+	AppEnvProd AppEnv = "prod"
+)
+
+var Environments = []AppEnv{AppEnvDev, AppEnvTest, AppEnvProd}
+
+type AppEnv string
+
+type Config struct {
+	Addr        string
+	Debug       bool
+	Environment AppEnv
+	FrontendURL string
+	LogLevel    int
+	DBConfig    DBConfig
+	AuthConfig  AuthConfig
+}
+
+type DBConfig struct {
+	Addr         string
+	MaxOpenConns int
+	MaxIdleConns int
+	MaxIdleTime  string
+}
+
+type AuthConfig struct {
+	Secret   string
+	Issuer   string
+	Audience string
+	Exp      int
+}
+
+func NewConfig() Config {
+	environment := AppEnv(utils.GetString("SERVER_ENVIRONMENT", string(AppEnvProd)))
+	if !slices.Contains(Environments, environment) {
+		environment = AppEnvProd
+	}
+
+	dbAddr := "DB_ADDR"
+	if environment == AppEnvTest {
+		dbAddr = "TEST_DB_ADDR"
+	}
+
+	cfg := Config{
+		Addr:        utils.GetString("SERVER_ADDR", ""),
+		Debug:       utils.GetBool("DEBUG", false),
+		Environment: environment,
+		FrontendURL: utils.GetString("FRONTEND_URL", ""),
+		LogLevel:    utils.GetInt("LOG_LEVEL", int(zerolog.InfoLevel)),
+		DBConfig: DBConfig{
+			Addr:         utils.GetString(dbAddr, ""),
+			MaxOpenConns: utils.GetInt("DB_MAX_OPEN_CONNS", 30),
+			MaxIdleConns: utils.GetInt("DB_MAX_IDLE_CONNS", 30),
+			MaxIdleTime:  utils.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+		AuthConfig: AuthConfig{
+			Secret:   utils.GetString("JWT_SECRET_KEY", ""),
+			Issuer:   utils.GetString("JWT_ISSUER", "meetup_clone"),
+			Audience: utils.GetString("JWT_AUDIENCE", "meetup_clone"),
+			Exp:      utils.GetInt("JWT_ACCESS_EXP", 3),
+		},
+	}
+
+	return cfg
+}
