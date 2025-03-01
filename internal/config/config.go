38397c27c407e506cdf35eb868666a6d9b33e13a
KengoWada <louiskengo3@gmail.com>
2025-03-01 17:51:56 +0300

    Add documentation for config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,6 @@
+// Package config provides application configuration settings
+// that are used throughout the application. It centralizes the management
+// of environment-specific values and parameters for consistency and ease of maintenance.
 package config
 
 import (
@@ -7,42 +10,56 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// Valid environment values for AppEnv.
 const (
-	AppEnvDev  AppEnv = "dev"
-	AppEnvTest AppEnv = "test"
-	AppEnvProd AppEnv = "prod"
+	AppEnvDev  AppEnv = "dev"  // Development environment
+	AppEnvTest AppEnv = "test" // Test environment
+	AppEnvProd AppEnv = "prod" // Production environment
 )
 
+// Valid environment values for AppEnv.
 var Environments = []AppEnv{AppEnvDev, AppEnvTest, AppEnvProd}
 
+// AppEnv represents the application environment as a string.
+// It ensures the environment is one of the following valid values:
+//   - "dev"  → Development environment
+//   - "test" → Testing environment
+//   - "prod" → Production environment
 type AppEnv string
 
+// Config holds the application configuration settings.
 type Config struct {
-	Addr        string
-	Debug       bool
-	Environment AppEnv
-	FrontendURL string
-	ApiURL      string
-	LogLevel    int
-	SecretKey   string
-	DBConfig    DBConfig
-	AuthConfig  AuthConfig
+	Addr        string     // The application port in the format ":8000".
+	Debug       bool       // Run the application in debug mode if true.
+	Environment AppEnv     // The environment the app is running in (e.g., "dev", "test", "prod").
+	FrontendURL string     // The URL of the frontend application.
+	ApiURL      string     // The API URL, should match the Addr. (e.g., "localhost:8000").
+	LogLevel    int        // The log level for the app.
+	SecretKey   string     // The secret key used for generating and signing tokens.
+	DBConfig    DBConfig   // The application database configurations
+	AuthConfig  AuthConfig // The application authentication configurations.
 }
 
+// DBConfig holds the database connection configuration settings.
 type DBConfig struct {
-	Addr         string
-	MaxOpenConns int
-	MaxIdleConns int
-	MaxIdleTime  string
+	Addr         string // The database connection string.
+	MaxOpenConns int    // The maximum number of open connections to the database.
+	MaxIdleConns int    // The maximum number of idle connections to keep in the pool.
+	MaxIdleTime  string // The maximum amount of time a connection can remain idle (e.g., "5m", "30s").
 }
 
+// AuthConfig holds the configuration settings for authentication and JWT handling.
 type AuthConfig struct {
-	Secret   string
-	Issuer   string
-	Audience string
-	Exp      int
+	Secret   string // The secret key used to sign and verify JWT tokens.
+	Issuer   string // The issuer claim (iss) for the tokens.
+	Audience string // The audience claim (aud) for the tokens.
+	Exp      int    // The token expiration time in hours.
 }
 
+// NewConfig creates a new Config instance with default values.
+//
+// Returns:
+//   - Config: a Config struct with initialized default settings.
 func NewConfig() Config {
 	environment := AppEnv(utils.GetString("SERVER_ENVIRONMENT", string(AppEnvProd)))
 	if !slices.Contains(Environments, environment) {
