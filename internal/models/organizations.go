041cef10be23bc47cb85660fee69ba6437edf577
KengoWada <louiskengo3@gmail.com>
2025-03-09 22:34:36 +0300

    Add organization models

---
+++ internal/models/organizations.go
@@ -0,0 +1,28 @@
+package models
+
+type Organization struct {
+	BaseModel
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ProfilePic  string `json:"profilePic"`
+	IsActive    bool   `json:"isActive"`
+}
+
+type Role struct {
+	BaseModel
+	Name           string        `json:"name"`
+	Description    string        `json:"description"`
+	OrganizationID int64         `json:"organizationId"`
+	Organization   *Organization `json:"organization"`
+	Permissions    []string      `json:"permissions"`
+}
+
+type OrganizationMember struct {
+	BaseModel
+	OrganizationID int64         `json:"organizationId"`
+	Organization   *Organization `json:"organization"`
+	UserProfileID  int64         `json:"userProfileId"`
+	UserProfile    *UserProfile  `json:"userProfile"`
+	RoleID         int64         `json:"roleId"`
+	Role           *Role         `json:"role"`
+}
