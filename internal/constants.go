99a91ed0a9971c6c8c2300eb39a50d9b3dc4ef7e
KengoWada <louiskengo3@gmail.com>
2025-03-09 22:37:32 +0300

    Add organization permission constants

--- internal/constants.go
+++ internal/constants.go
@@ -10,4 +10,43 @@ const (
 	DateFormat = "01/02/2006"
 
 	UserCtx userKey = "user"
+
+	// Event permissions
+	EventCreate  = "create_event"
+	EventPublish = "publish_event"
+	EventUpdate  = "update_event"
+	EventCancel  = "cancel_event"
+	EventDelete  = "delete_event"
+
+	// Member permissions
+	MemberAdd        = "add_member"
+	MemberRemove     = "remove_member"
+	MemberRoleUpdate = "update_members_role"
+
+	// Role permissions
+	RoleCreate = "create_role"
+	RoleUpdate = "update_role"
+	RoleDelete = "delete_role"
+
+	// Organization permissions
+	OrgUpdate     = "update_org"
+	OrgDeactivate = "deactivate_org"
+	OrgDelete     = "delete_org"
+)
+
+var (
+	Permissions = []string{
+		EventCreate, EventPublish, EventUpdate,
+		EventCancel, EventDelete,
+		MemberAdd, MemberRemove, MemberRoleUpdate,
+		RoleCreate, RoleUpdate, RoleDelete,
+		OrgUpdate, OrgDeactivate, OrgDelete,
+	}
+
+	PermissionsMap = map[string][]string{
+		"events":        {EventCreate, EventPublish, EventUpdate, EventCancel, EventDelete},
+		"members":       {MemberAdd, MemberRemove, MemberRoleUpdate},
+		"roles":         {RoleCreate, RoleUpdate, RoleDelete},
+		"organizations": {OrgUpdate, OrgDeactivate, OrgDelete},
+	}
 )
