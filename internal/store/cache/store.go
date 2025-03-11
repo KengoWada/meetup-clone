8a3650e4c4b5c92a17c2b81de574172382c88b48
KengoWada <louiskengo3@gmail.com>
2025-03-11 21:28:31 +0300

    Add getFromCache

--- internal/store/cache/store.go
+++ internal/store/cache/store.go
@@ -6,8 +6,17 @@ import (
 )
 
 const (
-	CacheKeyUser string = "user:"
+	CacheKeyUser string = "user"
 	CacheTTLUser int32  = 60 * 60 // 1 hour in seconds
+
+	CacheKeyOrg string = "org"
+	CacheTTLOrg int32  = 60 * 60 // 1 hour in seconds
+
+	CacheKeyRole string = "role"
+	CacheTTLRole int32  = 60 * 60 // 1 hour in seconds
+
+	CacheKeyOrgMember string = "org_member"
+	CacheTTLOrgMember int32  = 60 * 60 // 1 hour in seconds
 )
 
 type CacheKey string
@@ -24,4 +33,17 @@ func NewCacheStore(memcached *memcache.Client) Store {
 	return Store{
 		Users: &UserStore{cacheDB: memcached},
 	}
+}
+
+func getFromCache(cache *memcache.Client, cacheKey string) (*memcache.Item, error) {
+	item, err := cache.Get(cacheKey)
+	if err != nil {
+		if err == memcache.ErrCacheMiss {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return item, nil
 }
