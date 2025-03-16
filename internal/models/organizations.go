162367bc4b0233fa7bc7aa32059e4f9fdf85b6d2
KengoWada <louiskengo3@gmail.com>
2025-03-16 21:25:58 +0300

    Add organization invite model

--- internal/models/organizations.go
+++ internal/models/organizations.go
@@ -40,3 +40,15 @@ type OrganizationMember struct {
 	RoleID         int64         `json:"roleId"`
 	Role           *Role         `json:"role"`
 }
+
+type OrganizationInvite struct {
+	BaseModel
+	OrganizationID int64         `json:"organizationId"`
+	Organization   *Organization `json:"organization"`
+	UserProfileID  int64         `json:"userProfileId"`
+	UserProfile    *UserProfile  `json:"userProfile"`
+	RoleID         int64         `json:"roleId"`
+	Role           *Role         `json:"role"`
+	AcceptedAt     *string       `json:"acceptedAt"`
+	DeclinedAt     *string       `json:"declinedAt"`
+}
